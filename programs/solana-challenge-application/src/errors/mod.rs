4d5ecf286244a66b4abd3dbd575eb11dbcede983
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-06-11 01:14:21 +0545

    chore: add ErrCode enum for program errors

---
+++ programs/solana-challenge-application/src/errors/mod.rs
@@ -0,0 +1,17 @@
+use anchor_lang::prelude::*;
+
+#[error_code]
+pub enum ErrCode {
+    #[msg("Challange count overflowed.")]
+    ChallangeCountOverflowed,
+    #[msg("Description may not exceed 256 characters.")]
+    DescriptionTooLong,
+    #[msg("Image URL may not exceed 120 characters.")]
+    ImageUrlTooLong,
+    #[msg("Location may not exceed 20 characters.")]
+    LocationTooLong,
+    #[msg("Reward amount must be greater than zero.")]
+    InvalidRewardAmount,
+    #[msg("Unknown reward type.")]
+    InvalidRewardType,
+}
