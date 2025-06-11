efa81b8e724cdfb67bf63202165ca76aafaec024
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-06-12 03:36:56 +0545

    icon: Add new customized favicon.

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter} from "next/font/google";
 import "./globals.css";
 import AppProvider from "./providers"; 
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
  
 const inter = Inter({ subsets: ["latin"] });
- 
+
 export const metadata: Metadata = {
-  title: "Okto React SDK with Google Auth",
-  description: "Next.js app integrated with Okto SDK and Google Authentication",
+  title: "Proof of Adventure",
+  // description: "Next.js app integrated with Okto SDK and Google Authentication",
+  icons: {
+    icon: './logo.png'
+  }
 };
  
 export default async function RootLayout({
