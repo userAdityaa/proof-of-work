1b746cd96f7d7fe408ad7d5a52b41a48ed45e66e
userAdityaa <aditya.chaudhary1558@gmail.com>
2025-06-15 04:12:07 +0545

    store: Put all state in one store.

--- app/src/app/store/store.ts
+++ app/src/app/store/store.ts
@@ -1,14 +1,16 @@
 import { create } from "zustand";
 
 type WalletState = {
-  provider: any;
-  setProvider: (provider: any) => void;
+  provider: unknown;
+  setProvider: (provider: unknown) => void;
   publicKey: string | null;
   setPublicKey: (key: string | null) => void;
   avatarUrl: boolean | null;
   setAvatarUrl: (avatar: boolean) => void;
   connected: boolean | null;
   setWalletConnection: (connected: boolean) => void;
+  userExists: boolean | null;
+  setUserExists: (userExists: boolean) => void;
 };
 
 export const useWalletStore = create<WalletState>((set) => ({
@@ -19,5 +21,7 @@ export const useWalletStore = create<WalletState>((set) => ({
   avatarUrl: null, 
   setAvatarUrl: (avatar) => set({ avatarUrl: avatar }),
   connected: null, 
-  setWalletConnection: (connected) => set({ connected: connected })
+  setWalletConnection: (connected) => set({ connected: connected }), 
+  userExists: null,
+  setUserExists: (userExists) => set({userExists: userExists}),
 }));
